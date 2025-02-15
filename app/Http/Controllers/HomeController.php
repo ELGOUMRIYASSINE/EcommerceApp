bd210b9010374d6bd8484bbb1a88646ef873a3c7
yassine <proowork15@gmail.com>
2025-02-15 20:48:18 +0100

    Multi Authentication Admin & User Login System jetstream|livewire

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
+
+class HomeController extends Controller
+{
+    public function redirect(){
+
+        // Retrieve the usertype from the authenticated User model to determine the redirection path
+        $usertype = Auth::User()->usertype;
+
+        if ($usertype == '1') {
+            return view('admin.home');
+        }
+        else {
+            return view('dashboard');
+        }
+
+    }
+}
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+
+    {{-- <h2>
+        Hello {{ Auth::user()->name }}
+    </h2> --}}
+
+</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>
+        Hello {{ Auth::user()->name }}
+    </h2>
+</body>
+</html>
